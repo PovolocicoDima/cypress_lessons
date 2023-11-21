d71c7fce1c7f84e9773536987b86ee2fac686559
Поволочко Дмитрий <povolochko@reg.ru>
2023-11-21 20:13:47 +0200

    setup & first tests

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,102 @@
+/// <reference types="cypress" />
+
+describe("our first suite", () => {
+  it("first test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    // by tag name
+    // cy.get("input");
+
+    // by id
+    cy.get("#inputEmail");
+
+    // by class name
+    cy.get(".input-full-width");
+  });
+
+  it("email and password labels", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid").then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
+      expect(emailLabelFirst).to.equal("Email");
+      expect(passwordLabelFirst).to.equal("Password");
+
+      cy.contains("nb-card", "Basic form").then((secondForm) => {
+        const passwordSecondText = secondForm
+          .find('[for="exampleInputPassword1"]')
+          .text();
+        expect(passwordLabelFirst).to.equal(passwordSecondText);
+        cy.wrap(secondForm)
+          .find('[for="exampleInputPassword1"]')
+          .should("contain", "Password");
+      });
+    });
+  });
+
+  it("invoke", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Basic form")
+      .find("nb-checkbox")
+      .click()
+      .find(".custom-checkbox")
+      .invoke("attr", "class")
+      .should("contain", "checked");
+  });
+
+  it("assert property", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Datepicker").click();
+
+    cy.contains("nb-card", "Common Datepicker")
+      .find("input")
+      .then((input) => {
+        cy.wrap(input).click();
+        cy.get("nb-calendar-day-picker").contains("17").click();
+        cy.wrap(input)
+          .invoke("prop", "value")
+          .should("contain", "Nov 17, 2023");
+      });
+  });
+
+  it("radio buttons", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should("be.checked");
+
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+
+        cy.wrap(radioButtons).eq(0).should("not.be.checked");
+
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it.only("checkboxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    cy.get('[type="checkbox"]').first().uncheck({ force: true });
+    cy.get('[type="checkbox"]').first().should("not.be.checked");
+  });
+});
