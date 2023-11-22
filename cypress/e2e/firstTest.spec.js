85790d183aea58d591b9da559bff8f045f5c9b44
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-11-22 12:53:00 +0200

    dialog boxes

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -142,7 +142,7 @@ describe("our first suite", () => {
     });
   });
 
-  it.only("web tables", () => {
+  it("web tables", () => {
     cy.visit("/");
     cy.contains("Tables & Data").click();
     cy.contains("Smart Table").click();
@@ -192,5 +192,35 @@ describe("our first suite", () => {
         }
       });
     });
+  });
+
+  it.only("dialog box", () => {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    // 1
+    // cy.get("tbody tr").first().find(".nb-trash").click();
+    // cy.on("window:confirm", (confirm) => {
+    //   expect(confirm).to.equal("Are you sure you want to delete?");
+    // });
+
+    // 2
+    // const stub = cy.stub();
+    // cy.on("window:confirm", stub);
+    // cy.get("tbody tr")
+    //   .first()
+    //   .find(".nb-trash")
+    //   .click()
+    //   .then(() => {
+    //     expect(stub.getCall(0)).to.be.calledWith(
+    //       "Are you sure you want to delete?"
+    //     );
+    //   });
+
+    // 3
+
+    cy.get("tbody tr").first().find(".nb-trash").click();
+    cy.on("window:confirm", () => false);
   });
 });
